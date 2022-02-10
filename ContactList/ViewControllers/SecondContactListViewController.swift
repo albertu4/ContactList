dd9430ac4749b192bbe01d7bae5cbd632278d701
Miwka <97275239+albertu4@users.noreply.github.com>
2022-02-11 03:05:26 +0900

    Corrected Font and backgroundColor of Sections in SecondVC. Finished!

--- ContactList/ViewControllers/SecondContactListViewController.swift
+++ ContactList/ViewControllers/SecondContactListViewController.swift
@@ -11,10 +11,22 @@ class SecondContactListViewController: UITableViewController {
     
     var persons: [Person] = []
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.sectionHeaderTopPadding = 0
+    }
+    
     // MARK: - Table view Sections title
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let contact = persons[section]
         return contact.fullName
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.textColor = .black
+        header.tintColor = .systemGray4
     }
     
     //MARK: - Table view Cells data
